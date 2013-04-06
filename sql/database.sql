e32a1ecfd71c053e9e9a102a3f27d634f7f79b3b
Jasneet Singh Bhatti <jazneetbhatti@gmail.com>
2013-04-06 23:00:45 +0530

    Added Readme and database schema. Also placed files in appropriate sub-directories

---
+++ sql/database.sql
@@ -0,0 +1,86 @@
+-- MySQL dump 10.13  Distrib 5.5.22, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: incomeTaxDB
+-- ------------------------------------------------------
+-- Server version	5.5.22-0ubuntu1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `faculty_details`
+--
+
+DROP TABLE IF EXISTS `faculty_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `faculty_details` (
+  `id` varchar(10) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `designation` varchar(40) DEFAULT NULL,
+  `year` int(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`,`year`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faculty_details`
+--
+
+LOCK TABLES `faculty_details` WRITE;
+/*!40000 ALTER TABLE `faculty_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `faculty_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `income_details`
+--
+
+DROP TABLE IF EXISTS `income_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `income_details` (
+  `id` varchar(10) DEFAULT NULL,
+  `month` varchar(10) DEFAULT NULL,
+  `pay_band` int(10) DEFAULT NULL,
+  `grade_pay` int(10) DEFAULT NULL,
+  `da` int(10) DEFAULT NULL,
+  `hra` int(10) DEFAULT NULL,
+  `ta` int(10) DEFAULT NULL,
+  `wardenship` int(10) DEFAULT NULL,
+  `arrear_salary` int(10) DEFAULT NULL,
+  `total` int(10) DEFAULT NULL,
+  `gpf` int(10) DEFAULT NULL,
+  `income_tax` int(10) DEFAULT NULL,
+  `group_insurance` int(10) DEFAULT NULL,
+  `prof_tax` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `income_details`
+--
+
+LOCK TABLES `income_details` WRITE;
+/*!40000 ALTER TABLE `income_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `income_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-04-06 22:32:03
